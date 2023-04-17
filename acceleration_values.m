6e26eb1684bf0a125fe119c42ba27c9c1a8fdd5b
bmoffit3 <moffit.13@osu.edu>
2023-04-17 14:34:52 -0400

    commented

--- acceleration_values.m
+++ acceleration_values.m
@@ -1,5 +1,17 @@
 function [an, alpha] = acceleration_values(vn,vt,w,c1,c2,a,b,m,J,u)
 
+% vn - normal velocity
+% vt - tangential velocity
+% w - angular velocity
+% c1 - front wheel cornering stiffness
+% c2 - rear wheel cornering stiffness
+% a - distance from front wheel to center of gravity
+% b - distance from front rear to center of gravity
+% m - mass of car + contents
+% J - mass moment of inertia around z axis
+% u - steering input
+
+
 %transverse acceleration
 an = ((-(c1+c2)*vn) + ((-a*c1)+((b*c2))*w))/(m*vt) + (c1*u)/m ;
 
